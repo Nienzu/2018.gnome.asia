1571f084a0686c76e021714782cdd504c9fd99c2
nienzu <ibqqz0602@gmail.com>
2018-05-18 21:20:27 +0800

    Update website:news and bg

--- src/components/about.vue
+++ src/components/about.vue
@@ -7,6 +7,10 @@
         <h4 style="background-color: black;color: white;padding: 20px 20px;">{{$t("About.news")}}</h4>
         </div>
         <p style="font-weight: 700;font-size: x-large">————</p>
+        <p style="font-weight: 600;font-size: medium;color:gray">May 18, 2018</p>
+        <a style="font-weight: 600;font-size: large;" href="https://www.gnome.org/news/2018/04/gnome-asia-summit-2018-call-for-papers-is-now-open/">GNOME.Asia Summit 2018 Call for Papers deadline is 5/25</a>
+        <p>Call for paper will be closed in 7 days.A standard session will be scheduled for 45 mins (35 mins talk + 10 mins Q&A). The session could be a technical talk, panel discussion, or BOF. If you need more time or additional resources, feel free to get in touch with the organizing team.
+          </p>
         <p style="font-weight: 600;font-size: medium;color:gray">April 27, 2018</p>
         <a style="font-weight: 600;font-size: large;" href="https://www.gnome.org/news/2018/04/gnome-asia-summit-2018-call-for-papers-is-now-open/">GNOME.Asia Summit 2018 Call for Papers is now open</a>
         <p>GNOME.Asia Summit 2018 invites proposals for presentations at the conference. GNOME.Asia Summit is the featured annual GNOME conference in Asia. It focuses primarily on the GNOME desktop, but also covers applications and the platform development tools. The summit brings together the GNOME community in Asia to provide a forum for users, developers, foundation leaders, governments 
