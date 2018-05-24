aa8ece6d65a72c6868ccbd8141103268dcdca102
sendz <sendzation@gmail.com>
2018-05-24 23:29:11 +0700

    Add widget for switcing language

---
+++ src/components/widgets/LanguageSwitcher.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul :id="id" class="dropdown-content tit1">
+      <li>
+        <a @click="() => switchLanguage('en')">English</a>
+      </li>
+      <li>
+        <a @click="() => switchLanguage('zhCHS')">简体中文</a>
+      </li>
+      <li>
+        <a @click="() => switchLanguage('zhCHT')">繁体中文</a>
+      </li>
+    </ul>
+</template>
+
+<script>
+export default {
+  props: ["id"],
+  methods: {
+    switchLanguage(locale) {
+      localStorage.lang = locale;
+      this.$i18n.locale = locale;
+      window.location.reload();
+    }
+  }
+};
+</script>
